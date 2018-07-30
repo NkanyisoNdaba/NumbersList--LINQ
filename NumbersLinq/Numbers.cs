1e3a3dabcbb5f248e7f1443a63401319765877cf
nkanyiso <ndaba.nkanyi@live.co.uk>
2018-07-30 08:57:23 +0200

    done

---
+++ NumbersLinq/Numbers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumbersLinq
+{
+    public class Numbers
+    {
+        public int Number { get; set; }
+
+    }
+}
---
+++ NumbersLinq/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.PerformanceData;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumbersLinq
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbersList = new List<Numbers>
+            {
+               new Numbers{Number = 2},
+                new Numbers{Number = 5},
+                new Numbers{Number = 2},
+                new Numbers{Number = 5},
+                new Numbers{Number = 7},
+                new Numbers{Number = 2},           
+            };
+
+            // var findNumbers = from Numbers in numbersList where Numbers.Number <= 9 && Numbers.Number>0 && Numbers.Number%2==0 select Numbers.Number;
+           // var findNumbers = from Numbers in numbersList where Numbers.Number <= 9 && Numbers.Number > 0 && Numbers.Number*Numbers.Number>20 select new {Number=Numbers.Number,Square= Numbers.Number * Numbers.Number};          
+            //var joinNumbers = string.Join("\n", findNumbers);
+
+            var findNumbers = (from Numbers in numbersList
+                where Numbers.Number.Equals(Numbers.Number)
+                select Numbers.Number).Count();
+
+            Console.WriteLine(findNumbers);
+            Console.ReadLine();
+        }
+    }
+}
